c98441fc400c4b0a18fdee10de4abc8ad3bc8654
renyujie <Ren19960316>
2023-09-11 16:54:44 +0800

    转发服务器

---
+++ src/views/hosts/deploy.vue
@@ -0,0 +1,9 @@
+<template>
+	配置
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
--- src/views/transition/transition.ts
+++ src/views/transition/transition.ts
@@ -2,11 +2,15 @@
 import { defHttp } from '/@/utils/http/axios';
 enum RoleApi {
   list = '/forwardServer/list', //所有
-  edit = ' ', //修改
+  edit = '/forwardServer/update', //修改
   add = '/forwardServer/add', //新增
-  del = ' ',//删除
+  del = '/forwardServer/delete',//删除
 }
 // 所有
 export const getlist = (params) => defHttp.post({ url: RoleApi.list, params });
 // 新增
 export const addlist = (params) => defHttp.post({ url: RoleApi.add, params });
+// 回显
+export const editlist = (params) => defHttp.post({ url: RoleApi.edit, params });
+// 删除
+export const dellist = (params) => defHttp.post({ url: RoleApi.del, params });
